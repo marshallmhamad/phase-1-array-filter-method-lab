932dc0fef8e0a56cce2365bdf18a8522641834ab
marshall <mhamad.marshall@gmail.com>
2022-10-23 19:14:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching (drivers, attribute) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === attribute.toLowerCase()
+    })
+}
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver){
+            return driver[0] === attribute[0]
+        })
+    }
+
+function matchName(drivers,attribute) {
+    return drivers.filter(function (driver) {
+        return driver.name === attribute
+    })
+}
